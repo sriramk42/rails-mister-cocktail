66cb2d99d94ade0b4fff035af60ec810b3e46800
Sriram Krishnan <sriramk1986@gmail.com>
2019-11-08 12:00:19 +0900

    Added photo field and added it in the create cocktail form

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -2,4 +2,5 @@ class Cocktail < ApplicationRecord
   has_many :doses, dependent: :destroy
   validates :name, presence: true, uniqueness: true
   has_many :ingredients, through: :doses
+  mount_uploader :photo, PhotoUploader
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,19 @@ list_parsed["drinks"].each do |value|
   Ingredient.create(name: value["strIngredient1"])
 end
 
+Cocktail.destroy_all
+
+url = "https://cdn.liquor.com/wp-content/uploads/2017/11/06100445/cosmopolitan-720x720-recipe.jpg"
+cocktail = Cocktail.new(name: "Cosmopolitan")
+cocktail.remote_photo_url = url
+cocktail.save
+
+url = "https://cdn.liquor.com/wp-content/uploads/2017/09/01105541/classic-bloody-mary-720x720-recipe.jpg"
+cocktail = Cocktail.new(name: "Bloody Mary")
+cocktail.remote_photo_url = url
+cocktail.save
+
+url = "https://cdn.liquor.com/wp-content/uploads/2017/11/06162353/singapore-sling-720x720-recipe.jpg"
+cocktail = Cocktail.new(name: "Singapore Sling")
+cocktail.remote_photo_url = url
+cocktail.save
